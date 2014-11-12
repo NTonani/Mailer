55b8a1173c22b69d9dc827befa27db77a417ccc7
NTonani <nathantonani@hotmail.com>
2014-11-12 10:42:21 -0800

    DB and Contact objects

---
+++ Database.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace HolidayMailerCSCD350
+{
+    class Database
+    {
+        private String connectionString;
+        private SQLiteConnection connection;
+        private SQLiteCommand command;
+        private String str;
+
+        public Database(String connectionString)
+        {
+            this.connectionString = connectionString;
+            this.connection = new SQLiteConnection(this.connectionString);
+            this.str = "";
+
+            connection.Open();
+        }
+
+        public void insert(string into, string values)
+        {
+            str = "INSERT INTO " + into + " VALUES @values";
+            command = new SQLiteCommand(str, connection);
+            command.Parameters.AddWithValue("@values", values);
+            command.ExecuteNonQuery();
+        }
+
+        public void select(string from, string values)
+        {
+            str = "SELECT " + from + " FROM @values";
+            command = new SQLiteCommand(str, connection);
+            command.Parameters.AddWithValue("@values", values);
+        }
+
+        public void select(string from, string values, string where)
+        {
+            str = "SELECT " + from + " FROM @values WHERE @where";
+            command = new SQLiteCommand(str, connection);
+            command.Parameters.AddWithValue("@values", values);
+        }
+
+        public void close()
+        {
+            connection.Close();
+        }
+
+    }
+}
