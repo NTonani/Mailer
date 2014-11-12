7d4e2babb100a672a5b1261ff0a488a30a750606
NTonani <nathantonani@hotmail.com>
2014-11-12 10:49:05 -0800

    Update Database.cs

--- Database.cs
+++ Database.cs
@@ -30,6 +30,18 @@ namespace HolidayMailerCSCD350
             command = new SQLiteCommand(str, connection);
             command.Parameters.AddWithValue("@values", values);
             command.ExecuteNonQuery();
+            /*This is how we are going to do the insert
+            NOTE: need to error check the email input
+            str = "INSERT INTO Contacts VALUES (@l,@f,@em,@prev,@rel,@dob)";
+                command = new SQLiteCommand(str, connection);
+                command.Parameters.AddWithValue("l", "t");
+                command.Parameters.AddWithValue("f", "n");
+                command.Parameters.AddWithValue("em", "nt@gmail.com");
+                command.Parameters.AddWithValue("prev", "1");
+                command.Parameters.AddWithValue("rel", "Family");
+                command.Parameters.AddWithValue("dob", "12/12/1990");
+                command.ExecuteNonQuery();
+            */
         }
 
         public void select(string from, string values)
