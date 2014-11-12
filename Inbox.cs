c90e48e2495fbf8cfec09bad6b986a2e72cebbaa
lburlingame <lelandburlingame@gmail.com>
2014-11-12 12:08:47 -0800

    Create Inbox.cs

---
+++ Inbox.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidayMailerCSCD350
+{
+    class Inbox
+    {
+        private String lname, fname, email, rel, DOB;
+        int prev;
+        public Inbox()
+        {
+        }
+        
+        public String ToString()
+        {
+            
+            return "";
+        }
+    }
+}
